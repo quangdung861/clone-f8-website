1de706e7f4271fe53533823609d636b73c931879
quangdung861 <quangdung861@gmail.com>
2023-04-29 10:12:51 +0700

    Update header and build file: jsconfig.json

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,6 @@
 export const ROUTES = {
   USER: {
     HOME: "/",
+    SEARCH: "/search",
   },
 };
--- src/layouts/user/UserLayout/index.jsx
+++ src/layouts/user/UserLayout/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+import Header from "../components/Header";
+
+import * as S from "./styles";
+
 const UserLayout = () => {
   return (
-    <div>
+    <S.Wrapper>
+      <Header />
       <Outlet />
-    </div>
+    </S.Wrapper>
   );
 };
 
